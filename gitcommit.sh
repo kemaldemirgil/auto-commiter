3a513253db7cd1cb6c01b14dd6ed5be8a4d1347a
kemaldemirgil <kemal.demirgil@hotmail.com>
2021-06-13 18:40:50 -0400

    updating script

--- gitcommit.sh
+++ gitcommit.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 RESET="\033[0m"
 # BOLD="\033[1m"
-CYAN="\033[1;36m"
+CYAN="\033[0;36m"
+echo -e "\e[0;33m<<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>> \e[0m"
 echo ""
-echo -e "\e[1;35m........Welcome to the auto-committer created by \e[1;30mgam3fac3\e[1;35m........\n \e[0m"
+echo -e "\e[1;35m        Welcome to the auto-committer created by \e[1;30mgam3fac3\e[1;35m        \n \e[0m"
+echo -e "\e[0;33m<<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>> \e[0m"
 git status  
 git add .
 read -p "$(echo -e $CYAN"Please enter a commit description --> "$RESET)" desc
 git commit -m "$desc"  
 git push origin main
-echo -e "\e[1;32m........Thanks for using the auto-commiter!........\n \e[0m"
+echo -e "\e[1;30m<<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>> \e[0m"
+echo ""
+echo -e "\e[0;33m                Thanks for using the auto-commiter!                \n \e[0m"
+echo -e "\e[1;30m<<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>> \e[0m"
